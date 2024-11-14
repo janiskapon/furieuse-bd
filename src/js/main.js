1fb6db166aff1e7cff8e229d8a93a4c9b65cbd8c
janiskapon <Janis.Kapon@studentfr.ch>
2024-11-14 17:02:33 +0100

    finalisation du header, size image, début des paralax, footer à finir. la section ''book'' est prête.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
-import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const viewportWidth = window.screen.width;
+const friseWidth = document.querySelector(".slider-element").offsetWidth;
+
+gsap.to(".slider-container", {
+  x: viewportWidth - friseWidth,
+  scrollTrigger: {
+    trigger: ".slider-mask",
+    start: "top top",
+    scrub: 1,
+    pin: true,
+  },
+});
