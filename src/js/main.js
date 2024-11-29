24ca43abc9691eaa2416736790e544e3c98d7dcc
janiskapon <Janis.Kapon@studentfr.ch>
2024-11-29 15:49:27 +0100

    parallax

--- src/js/main.js
+++ src/js/main.js
@@ -21,7 +21,7 @@ gsap.to(".paralax-baron-one", {
     start: "center bottom",
     end: "+=50%",
     scrub: 1,
-    markers: true,
+    // markers: true,
   },
   left: "10%",
 });
@@ -32,7 +32,40 @@ gsap.to(".paralax-fuite", {
     start: "center bottom",
     end: "+=20%",
     scrub: 1,
-    markers: true,
+    // markers: true,
   },
   right: "10%",
+});
+
+gsap.to(".paralax-danger", {
+  scrollTrigger: {
+    trigger: ".paralax-danger",
+    start: "center bottom",
+    end: "+=170%",
+    scrub: 1,
+    // markers: true,
+  },
+  left: "10%",
+});
+
+gsap.to(".paralax-baron-two", {
+  scrollTrigger: {
+    trigger: ".paralax-baron-two",
+    start: "center bottom",
+    end: "+=170%",
+    scrub: 1,
+    // markers: true,
+  },
+  left: "10%",
+});
+
+gsap.to(".paralax-sword", {
+  scrollTrigger: {
+    trigger: ".paralax-sword",
+    start: "center bottom",
+    end: "+=50% ",
+    scrub: 1,
+    markers: true,
+  },
+  left: "10%",
 });
